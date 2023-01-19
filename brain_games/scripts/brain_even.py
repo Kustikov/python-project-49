1364deea78df8fe9df4e848e6d818859156739cf
kustikov <pasha_k1987@mail.ru>
2023-01-19 03:04:07 +0300

    Add brain-even package

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from brain_games.choose_even import random_number, brain_even
+
+def main():
+    random_number()
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
