5d6c3ebd8fdfe41db2549ddb610b11bd8bb47eae
kustikov <pasha_k1987@mail.ru>
2023-02-03 16:57:36 +0300

    refactoring

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -4,22 +4,13 @@ from random import randint
 rules = "What is the result of the expression?"
 
 
-def random_expression():  # generate expression for calc
+def calculator():  # brain-calc
     num1 = randint(1, 30)
     num2 = randint(1, 10)
     index = randint(0, 2)
     operators = ["+", "-", "*"]
-    current_operator = operators[index]
-    result = f"{num1} {current_operator} {num2}"
-    return result
-
-
-def calculator():  # brain-calc
-    question = random_expression()
-    num_list = question.split(" ")  # str.answer from user to int
-    operand = num_list[1]
-    num1 = int(num_list[0])
-    num2 = int(num_list[2])
+    operand = operators[index]
+    question = f"{num1} {operand} {num2}"
     result = 0
     if operand == "*":
         result = num1 * num2
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,6 +7,7 @@ rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def brain_prime():
     number = randint(1, 30)
     question = number
+
     if number < 2:
         return question, "no"
     for i in range(2, int((number) / 2) + 1):
