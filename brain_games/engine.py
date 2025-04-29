1b66fd348e874cd88016270c0d576e195efb1cda
Pavel <pasha_k1987@mail.ru>
2025-04-29 13:31:57 +0300

    debug brain-even

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,7 +4,7 @@ from brain_games.cli import correct, welcome_user, win, wrong
 
 ROUND_COUNT = 3  # round count's
 
-
+"""
 def play_game(game_logic, rule):
     name = welcome_user()
     print(rule)
@@ -21,3 +21,28 @@ def play_game(game_logic, rule):
         elif user_answer != correct_answer:
             wrong(user_answer, correct_answer, name)
     win(name)
+"""
+def play_game(game_logic, rule):
+    name = welcome_user()
+    print(rule)
+
+    while True:  # Этот цикл будет повторяться, если игрок ошибается
+        i = 0
+        while i < ROUND_COUNT:
+            question, correct_answer = game_logic()
+            correct_answer = str(correct_answer)
+            print(f"Question: {question}")
+            user_answer = prompt.string("Your answer: ")
+            user_answer = user_answer.lower()
+
+            if user_answer == correct_answer:
+                correct()
+                i += 1
+            else:
+                wrong(user_answer, correct_answer, name)
+                break  # Выход из внутреннего цикла и перезапуск игры
+
+        # Если игрок прошёл все раунды, показываем выигрыш
+        if i == ROUND_COUNT:
+            win(name)
+            break  # Завершаем игру
